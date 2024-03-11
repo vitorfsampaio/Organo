5e9b65e2cb42726679808f8d8bd3da095644f230
Vitor <vitorguilherme286@gmail.com>
2024-03-11 17:33:21 -0300

    input select creado

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,7 +1,19 @@
 import CampoTexto from '../CampoTexto'
+import ListaSuspensa from '../ListaSuspensa'
 import './Formulario.css'
 
 const formulario = () => {
+
+    const times = [
+        'Programação',
+        'Front-End',
+        'Data-Science',
+        'DevOps',
+        'UX e Design',
+        'Mobile',
+        'Inovação e gestão'
+    ]
+
     return (
         <section className='formulario'>
             <form>
@@ -9,6 +21,7 @@ const formulario = () => {
                 <CampoTexto label="Nome" placeholder="Digite seu nome" />
                 <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
                 <CampoTexto label="Imagem" placeholder="Informe o endereço da imagem" />
+                <ListaSuspensa label="Time" itens={times}/>
             </form>
         </section>
     )
