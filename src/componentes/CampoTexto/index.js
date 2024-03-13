e357714fcaeebe10504d13dc7c5fe03e8cb6ef2a
Vitor <vitorguilherme286@gmail.com>
2024-03-13 15:21:10 -0300

    interações novas

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -1,10 +1,23 @@
+import { useState } from 'react'
 import './CampoTexto.css'
 
 const CampoTexto = (props) => {
+
+    //let valor = 'Guilherme'
+
+    const [] = useState('')
+
+    const aoDigitado = (evento) => {
+        valor = evento.target.value
+        console.log(valor);
+    }
+
     return (
         <div className="campo-texto">
-            <label>{props.label}</label>
-            <input required={props.obrigatorio} placeholder={props.placeholder}/>
+            <label>
+                {props.label}
+            </label>
+            <input value={valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={props.placeholder}/>
         </div>
     )
 }
