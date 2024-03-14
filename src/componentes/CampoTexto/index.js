b3d209819442478a9823fc13d15a9f9064144baa
Vitor <vitorguilherme286@gmail.com>
2024-03-13 21:43:09 -0300

    update

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -5,11 +5,10 @@ const CampoTexto = (props) => {
 
     //let valor = 'Guilherme'
 
-    const [] = useState('')
+    const [valor, setValor] = useState('Guilherme')
 
     const aoDigitado = (evento) => {
-        valor = evento.target.value
-        console.log(valor);
+        props.aoAlterado(evento)
     }
 
     return (
@@ -17,7 +16,7 @@ const CampoTexto = (props) => {
             <label>
                 {props.label}
             </label>
-            <input value={valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={props.placeholder}/>
+            <input value={props.valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={props.placeholder}/>
         </div>
     )
 }
